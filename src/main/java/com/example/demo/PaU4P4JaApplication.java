63f4ac3d0cc329617abfff4a92abfa1e6495955a
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2023-08-14 19:49:39 -0500

    Taller38

---
+++ src/main/java/com/example/demo/PaU4P4JaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU4P4JaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU4P4JaApplication.class, args);
+	}
+
+}
